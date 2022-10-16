69c31a0694197298861b2724e82412f147771cd4
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-16 17:41:45 -0500

    Add methods to handle two special cases
    Special cases:
        Highest prices is on the first day
        Lowest price is on the last day

--- main.rb
+++ main.rb
@@ -1,3 +1,17 @@
-def stock_picker(prices); end
+def stock_picker(prices)
+  prices_minmax = prices.minmax
+  prices_minmax = remove_first_day_max(prices, prices_minmax[1]) if prices_minmax[1] == prices[0]
+  prices_minmax = remove_last_day_min(prices, prices_minmax[0]) if prices_minmax[0] == prices[prices.length]
+end
+
+def remove_first_day_max(prices_array, max_value)
+  new_prices_array = prices_array.select { |value| value < max_value }
+  new_prices_array.minmax
+end
+
+def remove_last_day_min(prices_array, min_value)
+  new_prices_array = prices_array.select { |value| value > min_value }
+  new_prices_array.minmax
+end
 
 puts stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
