8a4e151600e082d9664ae95e2198389df9065327
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-16 17:41:45 -0500

    Restart from scratch
    This change represents the full logic of what the final method will hold
    
    Future commits will refactor the code to include methods to house this
    logic

--- main.rb
+++ main.rb
@@ -1,17 +1,24 @@
-def stock_picker(prices)
-  prices_minmax = prices.minmax
-  prices_minmax = remove_first_day_max(prices, prices_minmax[1]) if prices_minmax[1] == prices[0]
-  prices_minmax = remove_last_day_min(prices, prices_minmax[0]) if prices_minmax[0] == prices[prices.length]
+# def stock_picker(day_prices);
+
+
+# puts stock_picker([17, 3, 6, 9, 15, 8, 6, 10, 1])
+
+array = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+
+array_max = array.max(array.length)
+array_min = array.min(array.length)
+new_max = 0
+new_min = 0
+
+array_max.each do |max|
+  array_min.each do |min|
+    next if max == array[0] || min == array[-1]
+
+    if (array.index(max) > array.index(min)) && (max - min) > (new_max - new_min)
+      new_max = max
+      new_min = min
+    end
+  end
 end
 
-def remove_first_day_max(prices_array, max_value)
-  new_prices_array = prices_array.select { |value| value < max_value }
-  new_prices_array.minmax
-end
-
-def remove_last_day_min(prices_array, min_value)
-  new_prices_array = prices_array.select { |value| value > min_value }
-  new_prices_array.minmax
-end
-
-puts stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+puts [array.index(new_min), array.index(new_max)]
