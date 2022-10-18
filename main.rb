410a2535e0bb5c340d1f13139603bf4aafcd05fd
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-18 11:50:29 -0500

    Refactor code into one method

--- main.rb
+++ main.rb
@@ -1,24 +1,17 @@
-# def stock_picker(day_prices);
+def stock_picker(day_prices);
+  new_max = 0
+  new_min = 0
+  day_prices.max(day_prices.length).each do |max|
+    day_prices.min(day_prices.length).each do |min|
+      next if max == day_prices[0] || min == day_prices[-1]
 
-
-# puts stock_picker([17, 3, 6, 9, 15, 8, 6, 10, 1])
-
-array = [17, 3, 6, 9, 15, 8, 6, 1, 10]
-
-array_max = array.max(array.length)
-array_min = array.min(array.length)
-new_max = 0
-new_min = 0
-
-array_max.each do |max|
-  array_min.each do |min|
-    next if max == array[0] || min == array[-1]
-
-    if (array.index(max) > array.index(min)) && (max - min) > (new_max - new_min)
-      new_max = max
-      new_min = min
+      if (day_prices.index(max) > day_prices.index(min)) && (max - min) > (new_max - new_min)
+        new_max = max
+        new_min = min
+      end
     end
   end
+  [day_prices.index(new_min), day_prices.index(new_max)]
 end
 
-puts [array.index(new_min), array.index(new_max)]
+puts stock_picker([17, 3, 6, 9, 15, 8, 6, 10, 1])
